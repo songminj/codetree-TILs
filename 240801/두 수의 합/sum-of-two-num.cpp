6f905c7081a68f3eb4628fd0986151e68900125b
MJ <104004869+songminj@users.noreply.github.com>
2024-08-01 20:13:42 +0900

    Title: 두 수의 합, Time: 1068ms, Memory: 78MB, Status: Time Limit Exceed - CodeTree

---
+++ 240801/두 수의 합/sum-of-two-num.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    unordered_map<int, int> um;
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    for (int i = 0; i < n-1; i++){
+        for (int j = i+1; j < n; j++){
+            int key = v[i] + v[j];
+            if (um.find(key) != um.end()){
+                um[key]++;
+            } else{
+                um[key] = 1;
+            }
+        }
+    }
+    cout << um[k];
+    return 0;
+}
