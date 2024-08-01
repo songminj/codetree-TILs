e67bc9442ce01dfae268ac34e8dbb8ded36a95b7
MJ <104004869+songminj@users.noreply.github.com>
2024-08-01 23:40:50 +0900

    Title: 세 수의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240801/세 수의 합/sum-of-three-num.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+#define MAX_N 1000
+
+int n, k;
+int arr[MAX_N];
+unordered_map<int, int> um;
+
+int main() {
+    cin >> n >> k;
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+        um[arr[i]]++;
+    }
+    for (int i = 0; i < n; i++){
+        um[arr[i]]--;
+        for (int j = 0; j < i; j++){
+            if(um.count(k-arr[i]-arr[j]) > 0)
+                ans += um[k-arr[i]-arr[j]]
+        }
+    }
+    cout << ans;
+    return 0;
+}
