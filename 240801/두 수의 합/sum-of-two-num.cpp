b7a5dd77db85ac90fcf883227616edd086f47e0f
MJ <104004869+songminj@users.noreply.github.com>
2024-08-01 23:34:08 +0900

    Title: 두 수의 합, Time: 75ms, Memory: 3MB, Status: Passed - CodeTree

--- 240801/두 수의 합/sum-of-two-num.cpp
+++ 240801/두 수의 합/sum-of-two-num.cpp
@@ -7,25 +7,31 @@ int main() {
     int res = 0;
     cin >> n >> k;
     unordered_map<int, int> um;
-    unordered_map<int, bool> check;
 
     for (int i = 0; i < n; i++) {
-        int newk;
-        cin >> newk;
-        um[newk]++;
+        int num;
+        cin >> num;
+        um[num]++;
     }
-    for (const auto& elem : um){
+
+    for (const auto& elem : um) {
         int num = elem.first;
-        int con = k-num;
-        if (check.find(con) != check.end()){
-            if (num == con){
-                res += (um[num] * (um[num]-1))/2;
+        int complement = k - num;
+        
+        // complement가 um에 있는지 확인하고, 같은 숫자가 아닌 경우만 계산
+        if (um.find(complement) != um.end()) {
+            if (num == complement) {
+
+                res += (um[num] * (um[num] - 1)) / 2;
             } else {
-                res += um[num]*um[con];
+
+                res += um[num] * um[complement];
             }
+
+            um[complement] = 0;
         }
-        um[con] = 0;
     }
-    cout << res;
+
+    cout << res << endl;
     return 0;
 }
