f9b1da90ec6cf63b7542757557a350a15c0a7425
MJ <104004869+songminj@users.noreply.github.com>
2024-08-02 19:23:20 +0900

    Title: 정수 명령 처리 5, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240802/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int n;
+string c;
+int main() {
+    vector<int> v;
+    cin >> n;
+    for (int i = 0; i<n; i++){
+        cin >> c;
+        if (c == "size") {
+            int s = v.size();
+            cout << s << "\n";
+        } else {
+            int k;
+            cin >> k;
+            if(c == "push_back") {
+                v.push_back(k);
+            } else if (c == "pop_back") {
+                v.pop_back();
+            } else if (c == "get") {
+                cout << v[k-1] << "\n";
+            }
+        }
+    }
+    return 0;
+}
