d2ef1cea0de365ba7cfa59fe83f85419f4a6b8db
MJ <104004869+songminj@users.noreply.github.com>
2024-08-02 20:12:38 +0900

    Title: treemap 기본, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240802/treemap 기본/treemap-basic.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int n;
+string cmd;
+
+int main() {
+    cin >> n;
+    map<int, int> m;
+    for (int i = 0 ; i < n; i++){
+        cin >> cmd;
+        if (cmd == "add") {
+            int k, v;
+            cin >> k >> v;
+            m[k] = v;
+        } else if (cmd == "find") {
+            int k;
+            cin >> k;
+            cout << (m.find(k) != m.end() ? to_string(m[k]) : "None") << "\n";
+        } else if (cmd == "remove") {
+            int k;
+            cin >> k;
+            m.erase(k);
+        } else if (cmd == "print_list"){
+            map<int, int>::iterator it;
+            for (it = m.begin(); it != m.end(); it++){
+                cout << (it -> second) << " "; 
+            }
+            cout << "\n";
+        }
+    }
+    return 0;
+}
