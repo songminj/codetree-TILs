7f458a92299296c920dce74ebbb282e2a0e99106
MJ <104004869+songminj@users.noreply.github.com>
2024-08-08 08:51:41 +0900

    Title: 정수 n개의 합 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240808/정수 n개의 합 3/sum-of-n-integers-3.cpp
+++ 240808/정수 n개의 합 3/sum-of-n-integers-3.cpp
@@ -6,16 +6,18 @@ int num;
 int main() {
     cin >> n >> k;
     int res =0;
+    int grid[n+1][n+1] = {};
     int prefix_sum[n+1][n+1] = {};
-    prefix_sum[0][0] = 0;
-    for (int i = 0; i <= n; i++){
-        prefix_sum[i][0] = 0;
-        prefix_sum[0][i] = 0;
-    }
+
     for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    for (int i =1; i <= n; i++){
         for (int j = 1; j <= n; j++){
-            cin >> num;
-            prefix_sum[i][j] = num + prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1];
+            prefix_sum[i][j] = grid[i][j] + prefix_sum[i-1][j] +prefix_sum[i][j-1] - prefix_sum[i-1][j-1];
         }
     }
 
