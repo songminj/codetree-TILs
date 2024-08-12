3c95579dcc1d5100e7e1617fa4dcaa2e32e575b8
MJ <104004869+songminj@users.noreply.github.com>
2024-08-12 22:01:53 +0900

    Title: 숫자 빠르게 찾기, Time: 178ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240812/숫자 빠르게 찾기/find-number-fast.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binary_search(int* target, vector<int>* v){
+    int l = 0, r= v->size() -1;
+    while (l <= r){
+        int mid = (l+r)/2;
+        if ((*v)[mid] == *target){
+            return mid+1;
+        }
+
+        if((*v)[mid] > *target){
+            r = mid -1;
+        } else {
+            l = mid+1;
+        }
+    }
+    return -1;
+}
+
+int n, m, p;
+int main() {
+    cin >> n >> m;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+    for (int i = 0; i < m; i++){
+        cin >> p;
+        cout << binary_search(&p, &v) << endl;
+    }
+    return 0;
+}
