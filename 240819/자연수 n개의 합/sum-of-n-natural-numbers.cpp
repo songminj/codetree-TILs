72193cebb8771ff46832d5b744195f5e79ec8d5f
MJ <104004869+songminj@users.noreply.github.com>
2024-08-19 21:17:36 +0900

    Title: 자연수 n개의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240819/자연수 n개의 합/sum-of-n-natural-numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+long long l = 1;
+long long r, s;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> s;
+    r = s;
+
+    long long min_num = r+1;                      // 최소이므로, 답이 될 수 있는 값보다 더 큰 값으로 설정합니다.
+    while (l <= r) {                // [l, r]가 유효한 구간이면 계속 수행합니다.
+        long long mid = (l + r) / 2;      // 가운데 위치를 선택합니다.
+        if(mid * (mid + 1) / 2 >= s) {   // 1부터 n까지의 합이 100보다 같거나 크다면
+            r = mid - 1;               // 왼쪽에 조건을 만족하는 숫자가 더 있을 가능성 때문에 right를 바꿔줍니다.
+            min_num = min(min_num, mid);   // 답의 후보들 중 최솟값을 계속 갱신해줍니다.
+        }
+        else                               
+            l = mid + 1;                // 100보다 작은 경우라면 l를 바꿔줍니다.
+    }
+    cout << min_num-1;
+    return 0;
+}
